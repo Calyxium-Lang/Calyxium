57a1fe7527095106fa4a3ad819b49904b4fbeb55
PeterGriffinSr <Codezz-ops@protonmail.com>
2025-07-21 17:39:20 -0400

    added workflow

--- bin/calyxium.ml
+++ bin/calyxium.ml
@@ -8,7 +8,6 @@ open Calyxiumlib.Error
 open Calyxiumlib.Version
 open Calyxiumlib.Help
 open Calyxiumlib.Bytegen
-open Calyxiumlib.Opcode
 
 let parse_file ~flags file =
   let bytecode_file = Filename.remove_extension file ^ ".cxc" in
@@ -19,12 +18,6 @@ let parse_file ~flags file =
         ignore (typecheck_program [ ast ]);
         let bytecode = compile_stmt ast in
 
-        List.iter
-          (fun op ->
-            pp_opcode Format.str_formatter op;
-            let str = Format.flush_str_formatter () in
-            Printf.printf "%s\n" str)
-          bytecode;
         if List.mem "--emit-bytecode" flags then (
           save_bytecode_to_file bytecode_file bytecode;
           Printf.printf "Bytecode saved to %s\n" bytecode_file);
